2e00565c0816c15789f43c7a99201e4cbf2cd671
Philippe Warnon <phw@civadis.be>
2019-07-25 15:04:09 +0200

    cor imports

--- src/main/java/be/civadis/store/base/config/AxonConfiguration.java
+++ src/main/java/be/civadis/store/base/config/AxonConfiguration.java
@@ -7,22 +7,11 @@ import org.axonframework.extensions.amqp.eventhandling.spring.SpringAMQPMessageS
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.Message;
-import org.springframework.amqp.core.AmqpAdmin;
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Exchange;
-import org.springframework.amqp.core.ExchangeBuilder;
-import org.springframework.amqp.core.ExchangeTypes;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.QueueBuilder;
-import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.util.StringUtils;
 
-import be.civadis.tools.es.BaseEvent;
 import be.civadis.store.base.config.axon.CivadisAMQPMessageSource;
 
 /**
